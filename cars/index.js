348e6dba46cb7eec0a45042bc391bebe41421032
ji <jkim0107@gmail.com>
2019-12-02 20:59:33 -0800

    Created Header component

--- cars/index.js
+++ cars/index.js
@@ -1,5 +1,11 @@
 //import libraries
+import React from "react";
+import { Text, AppRegistry } from "react-native";
 
 //create component 
+const App = () => {
+    return <Text>Hi there</Text>;
+};
 
 //render component to screen
+AppRegistry.registerComponent("cars", () => App);
