838caef3606d2f541fae0170f495bd28db4e9194
ji <jkim0107@gmail.com>
2019-12-02 23:00:30 -0800

    Created axios request

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
 
 class CarList extends Component {
+  state = {
+    carList: []
+  };
+
   componentDidMount() {
-    console.log('console from car list component')
+    axios.get('https://givecars.herokuapp.com')
+      .then(response => {
+        this.setState({carList: response.data})
+      })
+      .catch(() => {
+        console.log(`error`)
+      })
+  }
+
+  renderList = () => {
+      return this.state.carList.map((brand) => {
+        return <Text>{brand.model[0].name}</Text>
+      });
   }
   render() {
     return (
       <View>
-        <Text>Car List</Text>
+        {this.renderList()}
       </View>
     );
   }
